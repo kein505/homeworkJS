ccc676fecb5ae497fd6db9f04c1ef91de6b0572b
Kein <kein1106@gmail.com>
2025-09-04 22:32:47 +0500

    add block5

---
+++ Block5/app.js
@@ -0,0 +1,16 @@
+const money = 100;
+const canBuy = money > 50;
+
+if(money > 50)
+{
+	console.log('Может купить наш продукт');
+}
+else if (money>5){
+	console.log('Куплен mini продукт');
+}
+
+else{
+	console.log('не хватает баланса');
+}
+
+console.log('Итог');
